0335aeba1b09d21fe01baaa26e05a38ee9ce43e5
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-02-25 20:20:00 +0200

    feat(frontend): Implement user creation handler (connects to backend)

---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,38 @@
+import { useMutation } from "react-query";
+//TODO: Fix the url import from .env file
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response) {
+      throw new Error("Failed to create a user!");
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
