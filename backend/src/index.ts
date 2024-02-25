ee3a5c1bf367a566c580a72d15d89ebed9630f5c
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-02-25 10:18:49 +0200

    feat(backend): Add /api/my/user API endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -16,10 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello" });
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
-  console.log("server is listening on localhost: 7000");
+  console.log("Server is listening on localhost: 7000");
 });
