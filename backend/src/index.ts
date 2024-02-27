4465f1140af82d2055bb6b29446d006f844d8806
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-02-27 17:59:56 +0200

    Implement deployment configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -16,6 +16,10 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
