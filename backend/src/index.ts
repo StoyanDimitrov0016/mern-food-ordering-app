4b065275b107066c0c24a6ab914a2737f9916dad
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-02-28 18:42:56 +0200

    feat(backend): Integrate Cloudinary for image management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+
 import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
@@ -12,6 +14,12 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
